6fb22a140a72e3e4b9c7d180ce9893b7405254c5
Agustin <agustincepeda1@gmail.com>
2023-05-01 12:57:06 -0300

    found jumbo bug

--- jumbo_links.py
+++ jumbo_links.py
@@ -14,24 +14,32 @@ import re
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import date
+from unidecode import unidecode
 
+#This funcs gives the section_urls
 
 #This funcs gives the section_urls
 
 def get_urls():
     #PATH = 'C:\Program Files (x86)\chromedriver.exe'
-
-    PATH= '/usr/lib/chromium-browser/chromedriver'
+    PATH = '/usr/lib/chromium-browser/chromedriver'
 
     link = 'https://www.jumbo.com.ar/'
 
     s= Service(PATH)
 
     driver=webdriver.Chrome(service=s)
+    #driver=webdriver.Firefox()
 
     driver.maximize_window()
+    #driver.minimize_window()
 
     driver.get(link)
+    
+    
+    #driver.execute_script("document.body.style.zoom='90%'") #minimize
+
+
     time.sleep(16)
 #cancel button
     #driver.find_element(By.ID,'onesignal-slidedown-cancel-button').click()
@@ -45,79 +53,36 @@ def get_urls():
 
     categories = driver.find_elements(By.CSS_SELECTOR,'li.vtex-menu-2-x-menuItem.vtex-menu-2-x-menuItem--menu-item-secondary')
 
+    
     print(f' the len of categories {len(categories)}')
-    
-    
 
-    links=[]
-    for i in range(1,len(categories)-1):
-        time.sleep(2)
-        categories = driver.find_elements(By.CSS_SELECTOR,'li.vtex-menu-2-x-menuItem.vtex-menu-2-x-menuItem--menu-item-secondary')
-        time.sleep(7)
-        #action=ActionChains(driver)
-        #action.move_to_element(categories[i]).perform()
-        driver.execute_script("arguments[0].scrollIntoView();", categories[i])
-        time.sleep(5)
-        #action.click(categories[i]).perform()
-        categories[i].click()
-        time.sleep(1)
-        url= driver.current_url
-        links.append(url)
-        time.sleep(6)
-        
-        menu_button=driver.find_element(By.CLASS_NAME,'pr9.items-stretch.vtex-flex-layout-0-x-stretchChildrenWidth')
-        time.sleep(3)
-        menu_button.click()
-        time.sleep(2)
-        #action.move_to_element(categories[14]).perform()
-        #driver.execute_script("arguments[0].scrollIntoView();", categories[14])
+    full_link=[]
+    categorias=[] 
+    for i in categories[1:]: #preprocessing porque con chrome webdriver no se pueden clickear algunas cosas
+        print(i.text)           #avoid first element cause it's different
+        categorias.append(i.text)       
+        elemento=unidecode(i.text).lower()
+        end_link= '?map=category-1'
+        if " " in elemento:
+            sub=elemento.split(" ")
+
+            substring="-".join(sub)
+            
+            full_link.append(link+substring+end_link) #sumo link y la parte de la categoria
+
+        else:
+            full_link.append(link+elemento+end_link)
+    
+    
 
     driver.quit()
 
-    return links
+    return categorias,full_link
 
+section_links= get_urls()
 
-section_links= get_urls() #applying the func to get section urls
+jumbo = pd.DataFrame({'categorias': section_links[0],'links':section_links[1]})
 
-
-#this funcs gives the func to analyze
-
-def urls_analyze(link):
-
-    links=link
-
-    #PATH = 'C:\Program Files (x86)\chromedriver.exe'
-
-    PATH= '/usr/lib/chromium-browser/chromedriver'
-
-
-
-    s= Service(PATH)
-
-    driver=webdriver.Chrome(service=s)
-
-    driver.maximize_window()
-
-    driver.get(links)
-    time.sleep(15)
-
-
-#cancel button
-    #driver.find_element(By.ID,'onesignal-slidedown-cancel-button').click()
-    time.sleep(3)
-    driver.find_element(By.CSS_SELECTOR,'div.categoryList-box')
-    driver.implicitly_wait(2)
-    driver.find_element(By.CSS_SELECTOR,'.categoryList__viewAll').click()
-    time.sleep(3)
-   
-
-    return driver.current_url
-
-
-getting_urls=[urls_analyze(i) for i in section_links] #applying the second func
-
-
-jumbo= pd.DataFrame({'links': getting_urls})
 
 today = date.today() #hold the date
 d1 = today.strftime("%Y%m%d") 
